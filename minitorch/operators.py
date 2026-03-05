7b81c18c9ca827578cdbfa2a3e95da15d4dfaadd
ArmsOpen <2161784340@qq.com>
2026-03-05 15:49:29 +0800

    Task 0.1: Operators

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -33,6 +33,133 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    "$f(x, y) = x * y$"
+    # TODO: Implement for Task 0.1.
+    return x * y
+
+
+def id(x: float) -> float:
+    "$f(x) = x$"
+    # TODO: Implement for Task 0.1.
+    return x
+
+
+def add(x: float, y: float) -> float:
+    "$f(x, y) = x + y$"
+    # TODO: Implement for Task 0.1.
+    return x + y
+
+
+def neg(x: float) -> float:
+    "$f(x) = -x$"
+    # TODO: Implement for Task 0.1.
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is less than y else 0.0"
+    # TODO: Implement for Task 0.1.
+    if x < y:
+        return 1.0
+    else:
+        return 0.0
+
+
+def eq(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is equal to y else 0.0"
+    # TODO: Implement for Task 0.1.
+    if x == y:
+        return 1.0
+    else:
+        return 0.0
+
+
+def max(x: float, y: float) -> float:
+    "$f(x) =$ x if x is greater than y else y"
+    # TODO: Implement for Task 0.1.
+    if x > y:
+        return x
+    else:
+        return y
+
+
+def is_close(x: float, y: float) -> float:
+    "$f(x) = |x - y| < 1e-2$"
+    # TODO: Implement for Task 0.1.
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    r"""
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
+
+    (See https://en.wikipedia.org/wiki/Sigmoid_function )
+
+    Calculate as
+
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+
+    for stability.
+    """
+    # TODO: Implement for Task 0.1.
+    if x >= 0:
+        return 1.0 / (1.0 + math.pow(math.e, -x))
+    else:
+        return math.pow(math.e, x) / (1.0 + math.pow(math.e, x))
+
+
+def relu(x: float) -> float:
+    """
+    $f(x) =$ x if x is greater than 0, else 0
+
+    (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
+    """
+    # TODO: Implement for Task 0.1.
+    if x > 0:
+        return x
+    else:
+        return 0.0
+
+
+EPS = 1e-6
+
+
+def log(x: float) -> float:
+    "$f(x) = log(x)$"
+    return math.log(x + EPS)
+
+
+def exp(x: float) -> float:
+    "$f(x) = e^{x}$"
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    r"If $f = log$ as above, compute $d \times f'(x)$"
+    # TODO: Implement for Task 0.1.
+    return d / x
+
+
+def inv(x: float) -> float:
+    "$f(x) = 1/x$"
+    # TODO: Implement for Task 0.1.
+    return 1 / x
+
+
+def inv_back(x: float, d: float) -> float:
+    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
+    # TODO: Implement for Task 0.1.
+    return -d / x ** 2
+
+
+def relu_back(x: float, d: float) -> float:
+    r"If $f = relu$ compute $d \times f'(x)$"
+    # TODO: Implement for Task 0.1.
+    if x > 0:
+        return d
+    else:
+        return 0.0
 
 
 # ## Task 0.3
